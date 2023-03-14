ac08c76f6b00c22c14486bcd92091b6a7ca986ae
Chepe Ruiz <chepe.ruiz@gmail.com>
2023-03-14 16:24:15 -0700

    Fixed start script

--- workshop_3/scripts/run_docker.sh
+++ workshop_3/scripts/run_docker.sh
@@ -1,14 +1,5 @@
 #/bin/bash
 
-#docker run -itu root:root \
-#    -p 8888:8888 \
-#    --device-cgroup-rule='c 189:* rmw' \
-#    --mount type=bind,source=/var/tmp,destination=/var/tmp \
-#    -v /dev/bus/usb:/dev/bus/usb \
-#    -v /home/chepe/hackerdojo/python-ml/workshop_3/notebooks:/home/openvino/openvino_tensorflow/examples/notebooks/notebooks \
-#    openvino/openvino_tensorflow_ubuntu20_runtime:2.2.0
-    #--device /dev/ion:/dev/ion \
-
 docker run -it \
     --device=/dev/dri \
     --group-add=$(stat -c "%g" /dev/dri/render* | head -n 1) \
